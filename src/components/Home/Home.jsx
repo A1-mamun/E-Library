32f41c2090362acbe92d5e184883ea7ebf5c337d
A1-mamun <1154almamun@gmail.com>
2024-03-26 07:50:23 +0600

    Home page books section complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
 import Banner from "../Banner/Banner";
+import Books from "../Books/Books";
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("Book.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
   return (
     <div>
       <Banner></Banner>
+      <Books books={books}></Books>
     </div>
   );
 };
---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,9 @@
+const Tag = ({ tag }) => {
+  return (
+    <h2 className="badge badge-lg p-4 bg-green-05 text-green font-medium">
+      {tag}
+    </h2>
+  );
+};
+
+export default Tag;
