0f91fe58d2cea2ef8d37bc1d57dcfa0640d3c206
A1-mamun <1154almamun@gmail.com>
2024-03-28 22:48:16 +0600

    Book Details page responsive

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -48,60 +48,66 @@ const BookDetails = () => {
   };
 
   return (
-    <div className=" mt-7 container mx-auto flex flex-col md:flex-row gap-20 h-[640px]">
-      <div className="bg-dark-05 rounded-2xl md:w-1/2 flex justify-center items-center p-10">
-        <img className="h-full" src={image} alt="Book" />
-      </div>
-      <div className="font-worksans md:w-1/2 flex flex-col">
-        <h1 className=" text-4xl font-bold font-playfair text-dark">
-          {bookName}
-        </h1>
-        <p className="text-xl font-medium text-dark-8 my-5">By : {author}</p>
-        <hr />
-        <p className="text-xl font-medium text-dark-8 py-5">{category}</p>
-        <hr />
-        <p className=" text-dark-7 py-7 grow">
-          <span className="font-bold text-dark">Review :</span> {review}
-        </p>
-        <div className="mb-7 flex gap-8">
-          <h4 className="font-bold text-dark">Tag</h4>
-          <div className="flex gap-2">
-            {tags.map((tag, idx) => (
-              <Tag key={idx} tag={tag}></Tag>
-            ))}
-          </div>
+    <div className="mt-7 container mx-auto  px-3 md:px-5 lg:px-0 ">
+      <div className="flex flex-col lg:flex-row gap-6 md:gap-10 lg:gap-16 lg:h-[680px] border border-dark-15 rounded-2xl p-5">
+        <div className="bg-dark-05 rounded-2xl lg:w-1/2 flex justify-center items-center p-10">
+          <img className=" md:h-[400px] lg:h-full" src={image} alt="Book" />
         </div>
-        <hr />
-        <div className="flex gap-16 my-6">
-          <div className="">
-            <h4 className=" text-dark-7">Number of Pages:</h4>
-            <h4 className=" text-dark-7">Publisher:</h4>
-            <h4 className=" text-dark-7">Year of Publishing:</h4>
-            <h4 className=" text-dark-7">Rating:</h4>
+        <div className="font-worksans lg:w-1/2 flex flex-col">
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold font-playfair text-dark">
+            {bookName}
+          </h1>
+          <p className="text-base md:text-xl font-medium text-dark-8 my-3 md:my-3 lg:my-4">
+            By : {author}
+          </p>
+          <hr />
+          <p className="text-base md:text-xl font-medium text-dark-8 py-2 md:py-3 lg:py-4">
+            {category}
+          </p>
+          <hr />
+          <p className="text-sm md:text-base text-dark-7 py-3 md:py-4 lg:py-5 grow">
+            <span className="font-bold text-dark">Review :</span> {review}
+          </p>
+          <div className=" mb-3 md:mb-4 lg:mb-5 flex flex-col md:flex-row md:items-center gap-5 md:gap-8">
+            <h4 className="font-bold text-dark">Tag</h4>
+            <div className="flex gap-2">
+              {tags.map((tag, idx) => (
+                <Tag key={idx} tag={tag}></Tag>
+              ))}
+            </div>
           </div>
-          <div>
-            <h4 className="text-dark font-semibold">{totalPages}</h4>
-            <h4 className="text-dark font-semibold">{publisher}</h4>
-            <h4 className="text-dark font-semibold">{yearOfPublishing}</h4>
-            <h4 className="text-dark font-semibold">{rating}</h4>
+          <hr />
+          <div className="flex gap-16 my-4 md:my-5 text-sm md:text-base">
+            <div className="">
+              <h4 className=" text-dark-7">Number of Pages:</h4>
+              <h4 className=" text-dark-7">Publisher:</h4>
+              <h4 className=" text-dark-7">Year of Publishing:</h4>
+              <h4 className=" text-dark-7">Rating:</h4>
+            </div>
+            <div>
+              <h4 className="text-dark font-semibold">{totalPages}</h4>
+              <h4 className="text-dark font-semibold">{publisher}</h4>
+              <h4 className="text-dark font-semibold">{yearOfPublishing}</h4>
+              <h4 className="text-dark font-semibold">{rating}</h4>
+            </div>
           </div>
-        </div>
-        <div className=" space-x-4">
-          <button
-            onClick={handleRead}
-            className="btn bg-white border border-dark-3 text-lg text-dark font-semibold"
-          >
-            Read
-          </button>
+          <div className=" space-x-4">
+            <button
+              onClick={handleRead}
+              className="btn btn-sm md:btn-md bg-white border border-dark-3 md:text-lg text-dark font-semibold"
+            >
+              Read
+            </button>
 
-          <button
-            onClick={handleWishlist}
-            className="btn bg-sky-blue-2 text-white text-lg font-semibold"
-          >
-            Wishlist
-          </button>
+            <button
+              onClick={handleWishlist}
+              className="btn btn-sm md:btn-md bg-sky-blue-2 text-white md:text-lg font-semibold"
+            >
+              Wishlist
+            </button>
+          </div>
+          <ToastContainer />
         </div>
-        <ToastContainer />
       </div>
     </div>
   );
